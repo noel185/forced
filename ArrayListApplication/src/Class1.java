503ac984e17ae4c079ab8eccae56e2e8bdc51f8c
noel <isp and computer login removed>
2021-08-06 20:45:20 -0400

    # WARNING: head commit changed in the meantime
    class project
    
    The Array Project from the finals test was used for this project
    exercise.
     the author name was replaced, because it referred to my full computer,
    computer login name and ISP that which I think for me is a big security
    concern

---
+++ ArrayListApplication/src/Class1.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		List<String> list = new ArrayList<String>();
+		String name = "";
+	
+		
+		for(int x=1; x <= 10; x++) {
+			System.out.println("Enter name number "+x+":");
+			name = scan.nextLine();
+			list.add(name);
+		}
+		
+		
+
+		//collection shuffle method
+        Collections.shuffle(list); 
+        //print the shuffled list
+        System.out.println("This is the shuffled List : " + list); 
+	}
+}
